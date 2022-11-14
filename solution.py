f7fb527713a0c159478a5dde0b96c130a0a1be09
RaahulMarr123 <112818989+RaahulMarr123@users.noreply.github.com>
2022-11-14 15:20:12 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,62 +1,169 @@
-# import socket module
 from socket import *
-# In order to terminate the program
+import os
 import sys
+import struct
+import time
+import select
+import binascii
+import pandas as pd
 
-def webServer(port=13331):
-  serverSocket = socket(AF_INET, SOCK_STREAM)
-  
-  #Prepare a server socket
-  serverSocket.bind(("", port))
-  
-  #Fill in start
+ICMP_ECHO_REQUEST = 8
+MAX_HOPS = 30
+TIMEOUT = 2.0
+TRIES = 1
+# The packet that we shall send to each router along the path is the ICMP echo
+# request packet, which is exactly what we had used in the ICMP ping exercise.
+# We shall use the same packet that we built in the Ping exercise
 
-  #Fill in end
+def checksum(string):
+# In this function we make the checksum of our packet
+    csum = 0
+    countTo = (len(string) // 2) * 2
+    count = 0
 
-  while True:
-    #Establish the connection
-    
-    print('Ready to serve...')
-    connectionSocket, addr = #Fill in start -are you accepting connections?     #Fill in end
-    
-    try:
-      message = #Fill in start -a client is sending you a message   #Fill in end 
-      filename = message.split()[1]
-      
-      #opens the client requested file. 
-      #Plenty of guidance online on how to open and read a file in python. How should you read it though if you plan on sending it through a socket?
-      f = open(filename[1:], #fill in start #fill in end)
-      #fill in end
-      
-      outputdata = b"Content-Type: text/html; charset=UTF-8\r\n"\
-      #Fill in start -This variable can store your headers you want to send for any valid or invalid request. 
-      #Content-Type above is an example on how to send a header as bytes
-      #Fill in end
+    while count < countTo:
+        thisVal = (string[count + 1]) * 256 + (string[count])
+        csum += thisVal
+        csum &= 0xffffffff
+        count += 2
 
-      #Send an HTTP header line into socket for a valid request. What header should be sent for a response that is ok? 
-      #Fill in start
+    if countTo < len(string):
+        csum += (string[len(string) - 1])
+        csum &= 0xffffffff
 
-      #Fill in end
+    csum = (csum >> 16) + (csum & 0xffff)
+    csum = csum + (csum >> 16)
+    answer = ~csum
+    answer = answer & 0xffff
+    answer = answer >> 8 | (answer << 8 & 0xff00)
+    return answer
 
-      #Send the content of the requested file to the client
-      for i in f: #for line in file
-        #Fill in start - send your html file contents #Fill in end 
-      connectionSocket.close() #closing the connection socket
-      
-    except Exception as e:
-      # Send response message for invalid request due to the file not being found (404)
-      #Fill in start
+def build_packet():
+    #Fill in start
+    # In the sendOnePing() method of the ICMP Ping exercise ,firstly the header of our
+    # packet to be sent was made, secondly the checksum was appended to the header and
+    # then finally the complete packet was sent to the destination.
 
-      #Fill in end
+    # Make the header in a similar way to the ping exercise.
+    # Append checksum to the header.
 
+    # Don’t send the packet yet , just return the final packet in this function.
 
-      #Close client socket
-      #Fill in start
+    myChecksum = 0
+    myID = os.getpid() & 0xFFFF
+    header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, myChecksum, myID, 1)
+    data = struct.pack("d", time.time())
+    myChecksum = checksum(header + data)
 
-      #Fill in end
+    if sys.platform == 'darwin':
+        # Convert 16-bit integers from host to network  byte order
+        myChecksum = htons(myChecksum) & 0xffff
+    else:
+        myChecksum = htons(myChecksum)
 
-  serverSocket.close()
-  sys.exit()  # Terminate the program after sending the corresponding data
+    header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, myChecksum, myID, 1)
+    #Fill in end
 
-if __name__ == "__main__":
-  webServer(13331)
+    # So the function ending should look like this
+
+    packet = header + data
+    return packet
+
+def get_route(hostname):
+    timeLeft = TIMEOUT
+    df = pd.DataFrame(columns=['Hop Count', 'Try', 'IP', 'Hostname', 'Response Code'])
+    break_check = False
+
+    for ttl in range(1,MAX_HOPS):
+        dict_df = {}
+        for tries in range(TRIES):
+            destAddr = gethostbyname(hostname)
+            #Fill in start
+            # Make a raw socket named mySocket
+            icmp = getprotobyname("icmp")
+            mySocket = socket(AF_INET, SOCK_RAW, icmp)
+            #Fill in end
+
+            mySocket.setsockopt(IPPROTO_IP, IP_TTL, struct.pack('I', ttl))
+            mySocket.settimeout(TIMEOUT)
+            try:
+                d = build_packet()
+                mySocket.sendto(d, (hostname, 0))
+                t= time.time()
+                startedSelect = time.time()
+                whatReady = select.select([mySocket], [], [], timeLeft)
+                howLongInSelect = (time.time() - startedSelect)
+                if whatReady[0] == []: # Timeout
+                    #Fill in start
+                    #append response to your dataframe including hop #, try #, and "Timeout" responses as required by the acceptance criteria
+                    dict_df["Try"] = tries+1
+                    dict_df["Hostname"] = "Timeout"
+                    dict_df["Response Code"] = "Timeout"
+                    #Fill in end
+                recvPacket, addr = mySocket.recvfrom(1024)
+                timeReceived = time.time()
+                timeLeft = timeLeft - howLongInSelect
+                if timeLeft <= 0:
+                    #Fill in start
+                    #append response to your dataframe including hop #, try #, and "Timeout" responses as required by the acceptance criteria
+                    dict_df["Try"] = tries+1
+                    dict_df["Hostname"] = "Timeout"
+                    dict_df["Response Code"] = "Timeout"
+                    #Fill in end
+                dict_df["Try"] = tries+1
+            except Exception as e:
+                print(e) 
+                continue
+            else:
+                #Fill in start
+                #Fetch the icmp type from the IP packet
+                header = recvPacket[20:28]
+                types, Code, Checksum, Id, Seq = struct.unpack('bbHHh', header)
+                dict_df["Response Code"] = str(types)
+                #Fill in end
+                try: #try to fetch the hostname
+                    #Fill in start
+                    dict_df["Hostname"] = gethostbyaddr(addr[0])[0]
+                    #Fill in end
+                except herror:   #if the host does not provide a hostname
+                    #Fill in start
+                    dict_df["Hostname"] = "Hostname not returnable"
+                    #Fill in end
+
+                if types == 11:
+                    bytes = struct.calcsize("d")
+                    timeSent = struct.unpack("d", recvPacket[28:28 +
+                    bytes])[0]
+                    #Fill in start
+                    #You should update your dataframe with the required column field responses here
+                    pass
+                    #Fill in end
+                elif types == 3:
+                    bytes = struct.calcsize("d")
+                    timeSent = struct.unpack("d", recvPacket[28:28 + bytes])[0]
+                    #Fill in start
+                    #You should update your dataframe with the required column field responses here
+                    pass
+                    #Fill in end
+                elif types == 0:
+                    bytes = struct.calcsize("d")
+                    timeSent = struct.unpack("d", recvPacket[28:28 + bytes])[0]
+                    #Fill in start
+                    #You should update your dataframe with the required column field responses here
+                    break_check = True
+                    #Fill in end
+                else:
+                    #Fill in start
+                    #If there is an exception/error to your if statements, you should append that to your df here
+                    pass
+                    #Fill in end
+                break
+        dict_df["IP"] = addr[0]
+        dict_df["Hop Count"] = ttl
+        df = df.append(dict_df, ignore_index=True)
+        if break_check:
+            break
+    return df
+
+if __name__ == '__main__':
+    df = get_route("google.co.il")
